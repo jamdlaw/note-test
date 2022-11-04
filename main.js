d05676a2c8a78f217394328e5f657b047f93d974
James Lawrence <jamdlaw@yahoo.com>
2022-11-04 13:46:42 -0700

    clean code up

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ const all_music_notes = ['A', 'A#', 'B', 'C', 'C#','D','D#','E', 'F','F#','G','G
 
 let note_display = document.getElementById('music_note');
 
-note_display.innerHTML = 'howdy';
-//console.log(all_music_notes.length);
 function getRandomNote(){
     let random_note = all_music_notes[Math.floor(Math.random()*all_music_notes.length)];
-    //console.log(random_note);
+    
 
     note_display.innerHTML = random_note;
     return random_note;
