d9721ab39cb0ff845ff94265c69fb556c55a5b4a
James Lawrence <jamdlaw@yahoo.com>
2022-11-04 14:26:20 -0700

    added start-stop button

--- main.js
+++ main.js
@@ -2,13 +2,43 @@ const all_music_notes = ['A', 'A#', 'B', 'C', 'C#','D','D#','E', 'F','F#','G','G
 
 let note_display = document.getElementById('music_note');
 
-function getRandomNote(){
-    let random_note = all_music_notes[Math.floor(Math.random()*all_music_notes.length)];
-    
+const mainButton = document.getElementById('main-btn');
+let interval;
 
+mainButton.addEventListener('click', () => { 
+    const action = mainButton.dataset;
+    
+    if (action.action === 'start'){
+        startGame();
+    }
+
+    else{
+        stopGame();
+    }
+
+});
+
+function startGame(){
+    getRandomNote();
+    mainButton.dataset.action = 'stop';
+    mainButton.textContent = 'Stop';
+
+    interval = setInterval(getRandomNote, 5000);
+}
+
+function stopGame(){
+    clearInterval(interval);
+    mainButton.dataset.action = 'start';
+    mainButton.textContent = 'Start';
+}
+
+function getRandomNote(){
+
+    let random_note = all_music_notes[Math.floor(Math.random()*all_music_notes.length)];
     note_display.innerHTML = random_note;
+    
     return random_note;
 }
-const interval = setInterval(getRandomNote, 5000);
 
-console.log(interval);
+
+
