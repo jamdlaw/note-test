c8a0aa161a35c2ce1b73504160ec645057201f58
James Lawrence <jamdlaw@yahoo.com>
2022-11-19 17:35:56 -0800

    fixed part of the timer start and stop

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const mainButton = document.getElementById('main-btn');
 const circleAnimation = document.getElementById('circle');
 const question_time = document.getElementById('question_time');
 const countdownNumberEl = document.getElementById('countdown-number');
-let interval;
+let interval, userCountDown,countdown,userCountDown2;
 
 mainButton.addEventListener('click', () => { 
     const action = mainButton.dataset;
@@ -20,28 +20,20 @@ mainButton.addEventListener('click', () => {
 
 });
 
-function startGame(){
-    
+function startGame(){  
     circleAnimation.style["-webkit-animation-duration"] = question_time.value + "s";
     circleAnimation.style.animationPlayState = 'running';
     getRandomNote();
     mainButton.dataset.action = 'stop';
     mainButton.textContent = 'Stop';
-
-    interval = setInterval(getRandomNote, question_time.value * 1000);
-    
-    
-    let countdown = question_time.value;
-
+    interval = setInterval(getRandomNote, question_time.value * 1000);   
+    countdown = question_time.value;
     countdownNumberEl.textContent = countdown;
-
-    setInterval(function() {
+    userCountDown = setInterval(function() {
     countdown = countdown >= 0 ? --countdown : 0;
     countdownNumberEl.textContent = countdown;
     }, 1000);
-
-    
-    setInterval(function() {
+    userCountDown2 = setInterval(function() {
         countdown = question_time.value;
         countdownNumberEl.textContent = question_time.value;
         }, question_time.value * 1000);
@@ -49,9 +41,15 @@ function startGame(){
 
 function stopGame(){
     clearInterval(interval);
+    clearInterval(userCountDown);
+    clearInterval(countdown);
+    clearInterval(userCountDown2);
     mainButton.dataset.action = 'start';
     mainButton.textContent = 'Start';
     circleAnimation.style.animationPlayState = 'paused';
+    circleAnimation.style.strokeDashoffset = 0; // tyring to reset the circle but does not see to work
+    circleAnimation.style["-webkit-animation-duration"] = question_time.value + "s";
+    
 }
 
 function getRandomNote(){
