3017b1bcc9c7018abad45cbb3b8602a79b11f8e4
James Lawrence <jamdlaw@yahoo.com>
2023-05-31 10:36:16 -0700

    changed veriable declaration from var to let, tested code to make sure it still worked

--- correlation_worker.js
+++ correlation_worker.js
@@ -1,9 +1,9 @@
 self.onmessage = function(event)
 {
-	var timeseries = event.data.timeseries;
-	var test_frequencies = event.data.test_frequencies;
-	var sample_rate = event.data.sample_rate;
-	var amplitudes = compute_correlations(timeseries, test_frequencies, sample_rate);
+	let timeseries = event.data.timeseries;
+	let test_frequencies = event.data.test_frequencies;
+	let sample_rate = event.data.sample_rate;
+	let amplitudes = compute_correlations(timeseries, test_frequencies, sample_rate);
 	self.postMessage({ "timeseries": timeseries, "frequency_amplitudes": amplitudes });
 };
 
@@ -11,16 +11,16 @@ function compute_correlations(timeseries, test_frequencies, sample_rate)
 {
 	// 2pi * frequency gives the appropriate period to sine.
 	// timeseries index / sample_rate gives the appropriate time coordinate.
-	var scale_factor = 2 * Math.PI / sample_rate;
-	var amplitudes = test_frequencies.map
+	let scale_factor = 2 * Math.PI / sample_rate;
+	let amplitudes = test_frequencies.map
 	(
 		function(f)
 		{
-			var frequency = f.frequency;
+			let frequency = f.frequency;
 
 			// Represent a complex number as a length-2 array [ real, imaginary ].
-			var accumulator = [ 0, 0 ];
-			for (var t = 0; t < timeseries.length; t++)
+			let accumulator = [ 0, 0 ];
+			for (let t = 0; t < timeseries.length; t++)
 			{
 				accumulator[0] += timeseries[t] * Math.cos(scale_factor * frequency * t);
 				accumulator[1] += timeseries[t] * Math.sin(scale_factor * frequency * t);
